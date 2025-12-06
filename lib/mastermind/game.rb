413eb8e6c9bd8568c7e95d129f17d9831b4ae075
LapTim <232677958+Fr0ggin@users.noreply.github.com>
2025-12-06 20:42:24 +0100

    add guess to Game to get input as guess from player

--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -2,9 +2,22 @@
 
 # Game fulfills all methods needed for running the game
 class Game
-  # game
-end
+  ALLOWED_INPUT = %w[R Y G B P W].freeze
 
+  def guess
+    puts 'Enter your guess'
+    loop do
+      guess = gets.chomp.split('')
+      if guess.length == 4 && guess.all? do |v|
+        ALLOWED_INPUT.include?(v)
+      end
+        return guess
+      end
+
+      puts 'Invalid unput, pick 4 pins'
+    end
+  end
+end
 
 # Class Game
 
